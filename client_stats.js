29cb1680c86aaec3e695eb39c9bd95bf26465ef3
colin mcgovern <colinmcg@udel.edu>
2020-09-06 19:27:02 -0400

    testing stats

--- client_stats.js
+++ client_stats.js
@@ -1,17 +1,48 @@
-var words_correct = new Array();
-if(Cookies.get('words_correct') != null){
-	words_correct = JSON.parse(Cookies.get('words_correct'));
+translations = new Array();
+$.getJSON('https://raw.githubusercontent.com/clem109/hsk-vocabulary/master/hsk-vocab-json/hsk-level-1.json', function (data) {
+
+	translations = data;
+
+	function id_type_to_text(id, type){
+		if(type=='hanzi'){
+			return translations[id]['hanzi'];
+		}
+		if(type=='pinyin'){
+			return translations[id]['pinyin'];
+		}
+		if(type=='translation'){
+			return translations[id]['translations'][0];
+		}
+	}
+
+	var words_wrong = new Array();
+	if(Cookies.get('words_wrong') != null){
+		words_wrong = JSON.parse(Cookies.get('words_wrong'));
+	}
+
+	var words_correct = new Array();
+	if(Cookies.get('words_correct') != null){
+		words_correct = JSON.parse(Cookies.get('words_correct'));
+	}
+
+	var words_wrong_string = "";
+	words_wrong.forEach( item => {
+		words_wrong_string.concat(id_type_to_text(item['id'],item['question']));
+		words_wrong_string.concat(' -> ');	
+		words_wrong_string.concat(id_type_to_text(item['id'],item['answer']));
+		words_wrong_string.concat('\n');
+
+	});
+	$("#dis_words_correct").text(words_wrong_string);
+
+	var words_correct_string = "";
+
+	words_correct.forEach( item => {
+		words_correct_string.concat(id_type_to_text(item['id'],item['question']));
+		words_correct_string.concat(' -> ');	
+		words_correct_string.concat(id_type_to_text(item['id'],item['answer']));
+		words_correct_string.concat('\n');
+	});
+	$("#dis_words_correct").text(words_correct_string);
+
 }
-
-var words_wrong = new Array();
-if(Cookies.get('words_wrong') != null){
-	words_wrong = JSON.parse(Cookies.get('words_wrong'));
-}
-
-words_correct.forEach( item => {
-	console.log(item);  
-});
-
-words_wrong.forEach( item => {
-	console.log(item);  
-});
