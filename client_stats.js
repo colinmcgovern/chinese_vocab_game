fa0b861b4253e7d044e848d267124d10c0e78c6d
colin mcgovern <colinmcg@udel.edu>
2020-09-08 18:41:28 -0400

    testing chosen words

--- client_stats.js
+++ client_stats.js
@@ -61,14 +61,14 @@ $.getJSON('https://raw.githubusercontent.com/clem109/hsk-vocabulary/master/hsk-v
 		var words_wrong_history_totals = new Array();
 
 		translations.forEach(item => {
-			words_wrong_history_totals[item['id']] = 0;
+			words_wrong_history_totals{item['id']} = 0;
 		});
 
 		print("words_wrong_history_totals when empty"); //del
 		print(words_wrong_history_totals); //del
 
 		words_wrong_history.forEach(item => {
-			words_wrong_history_totals[item['id']]++;
+			words_wrong_history_totals{item['id']}++;
 		});
 
 		print("words_wrong_history_totals"); //del
@@ -82,7 +82,7 @@ $.getJSON('https://raw.githubusercontent.com/clem109/hsk-vocabulary/master/hsk-v
 		print(words_wrong_history_totals); //del
 
 		Object.keys(words_wrong_history_totals).forEach(function(k){
-			if(chosen_words.length < 4 && words_wrong_history_totals[k]!=0){
+			if(chosen_words.length < 4 && words_wrong_history_totals{k}!=0){
 				chosen_words.push(k);
 			}
 		});
